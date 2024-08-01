9e132f8b0e88960db5e36574367b90d5122a3026
spkadyrov <sp.kadyrov@bftcom.com>
2024-08-01 21:46:23 +0500

    изменил код

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 #!/usr/bin/env python3
 
-from src.Osintgram import Osintgram
+import requests
+import json
+import sys
 import argparse
+import signal
+from src.Osintgram import Osintgram
 from src import printcolors as pc
 from src import artwork
-import sys
-import signal
 
 is_windows = False
 
 try:
-    import gnureadline  
-except: 
+    import gnureadline
+except ImportError:
     is_windows = True
     import pyreadline
-
 
 def printlogo():
     pc.printout(artwork.ascii_art, pc.YELLOW)
     pc.printout("\nVersion 1.1 - Developed by Giuseppe Criscione\n\n", pc.YELLOW)
     pc.printout("Type 'list' to show all allowed commands\n")
-    pc.printout("Type 'FILE=y' to save results to files like '<target username>_<command>.txt (default is disabled)'\n")
-    pc.printout("Type 'FILE=n' to disable saving to files'\n")
-    pc.printout("Type 'JSON=y' to export results to a JSON files like '<target username>_<command>.json (default is "
-                "disabled)'\n")
-    pc.printout("Type 'JSON=n' to disable exporting to files'\n")
-
+    pc.printout("Type 'FILE=y' to save results to files like '<target username>_<command>.txt' (default is disabled)\n")
+    pc.printout("Type 'FILE=n' to disable saving to files\n")
+    pc.printout("Type 'JSON=y' to export results to a JSON file like '<target username>_<command>.json' (default is disabled)\n")
+    pc.printout("Type 'JSON=n' to disable exporting to files\n")
 
 def cmdlist():
     pc.printout("FILE=y/n\t")
-    print("Enable/disable output in a '<target username>_<command>.txt' file'")
+    print("Enable/disable output in a '<target username>_<command>.txt' file")
     pc.printout("JSON=y/n\t")
-    print("Enable/disable export in a '<target username>_<command>.json' file'")
+    print("Enable/disable export in a '<target username>_<command>.json' file")
     pc.printout("addrs\t\t")
-    print("Get all registered addressed by target photos")
+    print("Get all registered addresses by target photos")
     pc.printout("cache\t\t")
     print("Clear cache of the tool")
     pc.printout("captions\t")
@@ -48,16 +47,12 @@ def cmdlist():
     print("Get users followed by target")
     pc.printout("fwersemail\t")
     print("Get email of target followers")
-    pc.printout("fwersnumber\t")
-    print("Get phone number of target followers")
-    pc.printout("fwersubset\t")
-    print("Get the list of users who follow both target1 and target2")
     pc.printout("fwingsemail\t")
     print("Get email of users followed by target")
+    pc.printout("fwersnumber\t")
+    print("Get phone number of target followers")
     pc.printout("fwingsnumber\t")
     print("Get phone number of users followed by target")
-    pc.printout("fwingsubset\t")
-    print("Get the list of users followed by both target1 and target2")        
     pc.printout("hashtags\t")
     print("Get hashtags used by target")
     pc.printout("info\t\t")
@@ -79,27 +74,23 @@ def cmdlist():
     pc.printout("target\t\t")
     print("Set new target")
     pc.printout("wcommented\t")
-    print("Get a list of user who commented target's photos")
+    print("Get a list of users who commented on target's photos")
     pc.printout("wtagged\t\t")
-    print("Get a list of user who tagged target")
-
+    print("Get a list of users who tagged target")
 
 def signal_handler(sig, frame):
     pc.printout("\nGoodbye!\n", pc.RED)
     sys.exit(0)
 
-
 def completer(text, state):
     options = [i for i in commands if i.startswith(text)]
     if state < len(options):
         return options[state]
-    else:
-        return None
+    return None
 
 def _quit():
     pc.printout("Goodbye!\n", pc.RED)
     sys.exit(0)
-
 
 signal.signal(signal.SIGINT, signal_handler)
 if is_windows:
@@ -109,11 +100,10 @@ else:
     gnureadline.parse_and_bind("tab: complete")
     gnureadline.set_completer(completer)
 
-parser = argparse.ArgumentParser(description='Osintgram is a OSINT tool on Instagram. It offers an interactive shell '
-                                             'to perform analysis on Instagram account of any users by its nickname ')
-parser.add_argument('id', type=str,  # var = id
-                    help='username')
-parser.add_argument('-C','--cookies', help='clear\'s previous cookies', action="store_true")
+parser = argparse.ArgumentParser(description='Osintgram is an OSINT tool on Instagram. It offers an interactive shell to perform analysis on Instagram account of any users by its nickname.')
+parser.add_argument('id', type=str, help='username')
+parser.add_argument('password', type=str, help='password')
+parser.add_argument('-C', '--cookies', help='clear previous cookies', action="store_true")
 parser.add_argument('-j', '--json', help='save commands output as JSON file', action='store_true')
 parser.add_argument('-f', '--file', help='save output in a file', action='store_true')
 parser.add_argument('-c', '--command', help='run in single command mode & execute provided command', action='store')
@@ -121,87 +111,121 @@ parser.add_argument('-o', '--output', help='where to store photos', action='stor
 
 args = parser.parse_args()
 
+def login_with_two_factor(session, username, password):
+    headers = {
+        'User-Agent': 'Instagram 10.26.0 Android'
+    }
 
-api = Osintgram(args.id, args.file, args.json, args.command, args.output, args.cookies)
+    data = {
+        'username': username,
+        'password': password,
+        'queryParams': {},
+        'optIntoOneTap': 'false'
+    }
 
+    login_response = session.post('https://i.instagram.com/api/v1/accounts/login/', data=data, headers=headers)
 
+    if login_response.status_code == 200:
+        print('Logged in successfully without 2FA')
+        return True
 
-commands = {
-    'list':             cmdlist,
-    'help':             cmdlist,
-    'quit':             _quit,
-    'exit':             _quit,
-    'addrs':            api.get_addrs,
-    'cache':            api.clear_cache,
-    'captions':         api.get_captions,
-    "commentdata":      api.get_comment_data,
-    'comments':         api.get_total_comments,
-    'followers':        api.get_followers,
-    'followings':       api.get_followings,
-    'fwersemail':       api.get_fwersemail,
-    'fwersnumber':      api.get_fwersnumber,
-    'fwersubset':       api.get_followers_subset,
-    'fwingsemail':      api.get_fwingsemail,
-    'fwingsnumber':     api.get_fwingsnumber,
-    'fwingsubset':      api.get_followings_subset,
-    'hashtags':         api.get_hashtags,
-    'info':             api.get_user_info,
-    'likes':            api.get_total_likes,
-    'mediatype':        api.get_media_type,
-    'photodes':         api.get_photo_description,
-    'photos':           api.get_user_photo,
-    'propic':           api.get_user_propic,
-    'stories':          api.get_user_stories,
-    'tagged':           api.get_people_tagged_by_user,
-    'target':           api.change_target,
-    'wcommented':       api.get_people_who_commented,
-    'wtagged':          api.get_people_who_tagged
-}
+    login_response_json = login_response.json()
 
+    if 'two_factor_required' in login_response_json:
+        print('Two-factor authentication is required')
+        two_factor_identifier = login_response_json['two_factor_info']['two_factor_identifier']
 
-signal.signal(signal.SIGINT, signal_handler)
-if is_windows:
-    pyreadline.Readline().parse_and_bind("tab: complete")
-    pyreadline.Readline().set_completer(completer)
-else:
-    gnureadline.parse_and_bind("tab: complete")
-    gnureadline.set_completer(completer)
+        verification_code = input('Enter the two-factor authentication code: ')
 
-if not args.command:
-    printlogo()
+        two_factor_data = {
+            'username': username,
+            'verification_code': verification_code,
+            'two_factor_identifier': two_factor_identifier,
+            'trust_this_device': '1',
+            'queryParams': {}
+        }
 
+        two_factor_response = session.post('https://i.instagram.com/api/v1/accounts/two_factor_login/', data=two_factor_data, headers=headers)
 
-while True:
-    if args.command:
-        cmd = args.command
-        _cmd = commands.get(args.command)
-    else:
-        signal.signal(signal.SIGINT, signal_handler)
-        if is_windows:
-            pyreadline.Readline().parse_and_bind("tab: complete")
-            pyreadline.Readline().set_completer(completer)
+        if two_factor_response.status_code == 200:
+            print('Logged in successfully with 2FA')
+            return True
         else:
-            gnureadline.parse_and_bind("tab: complete")
-            gnureadline.set_completer(completer)
-        pc.printout("Run a command: ", pc.YELLOW)
-        cmd = input()
-
-        _cmd = commands.get(cmd)
-
-    if _cmd:
-        _cmd()
-    elif cmd == "FILE=y":
-        api.set_write_file(True)
-    elif cmd == "FILE=n":
-        api.set_write_file(False)
-    elif cmd == "JSON=y":
-        api.set_json_dump(True)
-    elif cmd == "JSON=n":
-        api.set_json_dump(False)
-    elif cmd == "":
-        print("")
+            print('Failed to log in with 2FA')
+            return False
     else:
-        pc.printout("Unknown command\n", pc.RED)
+        print(f'Failed to log in. Status code: {login_response.status_code}')
+        return False
 
-    if args.command:
-        break
+session = requests.Session()
+
+if login_with_two_factor(session, args.id, args.password):
+    api = Osintgram(args.id, args.file, args.json, args.command, args.output, args.cookies)
+
+    commands = {
+        'list': cmdlist,
+        'help': cmdlist,
+        'quit': _quit,
+        'exit': _quit,
+        'addrs': api.get_addrs,
+        'cache': api.clear_cache,
+        'captions': api.get_captions,
+        'commentdata': api.get_comment_data,
+        'comments': api.get_total_comments,
+        'followers': api.get_followers,
+        'followings': api.get_followings,
+        'fwersemail': api.get_fwersemail,
+        'fwingsemail': api.get_fwingsemail,
+        'fwersnumber': api.get_fwersnumber,
+        'fwingsnumber': api.get_fwingsnumber,
+        'hashtags': api.get_hashtags,
+        'info': api.get_user_info,
+        'likes': api.get_total_likes,
+        'mediatype': api.get_media_type,
+        'photodes': api.get_photo_description,
+        'photos': api.get_user_photo,
+        'propic': api.get_user_propic,
+        'stories': api.get_user_stories,
+        'tagged': api.get_people_tagged_by_user,
+        'target': api.change_target,
+        'wcommented': api.get_people_who_commented,
+        'wtagged': api.get_people_who_tagged,
+    }
+
+    if not args.command:
+        printlogo()
+
+    while True:
+        if args.command:
+            cmd = args.command
+            _cmd = commands.get(args.command)
+        else:
+            signal.signal(signal.SIGINT, signal_handler)
+            if is_windows:
+                pyreadline.Readline().parse_and_bind("tab: complete")
+                pyreadline.Readline().set_completer(completer)
+            else:
+                gnureadline.parse_and_bind("tab: complete")
+                gnureadline.set_completer(completer)
+            pc.printout("Run a command: ", pc.YELLOW)
+            cmd = input()
+
+            _cmd = commands.get(cmd)
+
+        if _cmd:
+            _cmd()
+        elif cmd == "FILE=y":
+            api.set_write_file(True)
+        elif cmd == "FILE=n":
+            api.set_write_file(False)
+        elif cmd == "JSON=y":
+            api.set_json_dump(True)
+        elif cmd == "":
+            print("")
+        else:
+            pc.printout("Unknown command\n", pc.RED)
+
+        if args.command:
+            break
+else:
+    print("Login failed")
